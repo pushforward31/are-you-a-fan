b0a4d363344ae5c4ce3e81003edcd122eb8c0b83
James <J.Lemons2@gmail.com>
2018-04-14 06:13:25 -0400

    update1

--- src/App.js
+++ src/App.js
@@ -2,39 +2,46 @@ import React, { Component } from "react";
 import HeroList from "./components/HeroList";
 import Wrapper from "./components/Wrapper";
 import Titlebox from "./components/Titlebox";
-import friends from "./friends.json";
+import heroes from "./heroes.json";
 import "./App.css";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    heroes
   };
 
   removeFriend = id => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
+    const heroes = this.state.heroes.map(hero => hero.id !== id);
     // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
+    this.setState({ heroes });
   };
 
   // Map over this.state.friends and render a HeroList component for each friend object
   render() {
     return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
+      <div className = "cover">
+        <Wrapper>
+          <header className= "title">
+            <Titlebox>Heroes List</Titlebox>
+          </header>
+
+        {this.state.heroes.map(hero => (
           <HeroList
             removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={hero.id}
+            key={hero.id}
+            name={hero.name}
+            image={hero.image}
+            occupation={hero.occupation}
+            location={hero.location}
           />
         ))}
+        
       </Wrapper>
+
+    </div>
     );
   }
 }
--- src/components/Titlebox/Titlebox.js
+++ src/components/Titlebox/Titlebox.js
@@ -3,8 +3,7 @@ import "./Titlebox.css";
 
 const Titlebox = props => (
 <div className = "jumbotron">
-	<h1 className="title">{props.children}</h1>;
-
-
+	<h1 className="title">{props.children}</h1>
 </div>	
+);
 export default Titlebox;
